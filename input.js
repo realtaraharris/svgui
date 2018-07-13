d13f744fe95bb3402b44734af395d1b3b6e9da4f
Max Harris <harris.max@gmail.com>
2018-07-13 10:37:00 -0500

    new, better key event handling strategy

--- input.js
+++ input.js
@@ -6,49 +6,40 @@ class InputController extends React.Component {
     this.state = {
       displayValue: ''
     }
-
-    this.onKeyUp = this.onKeyUp.bind(this)
   }
 
-  onKeyUp ({ charCode, keyCode }) {
-    if (keyCode === 8) {
-      const displayValue = this.state.displayValue.slice(0, this.state.displayValue.length - 1)
-      console.log('displayValue:', displayValue)
-      this.setState({ displayValue })
-    } else {
-      console.log('charCode:', charCode)
-      this.setState({ displayValue: this.state.displayValue + charCode })
-    }
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.keystroke === undefined) { return }
+    this.setState({ displayValue: this.state.displayValue + nextProps.keystroke })
   }
 
   render () {
-    return <Input {...this.props} onKeyUp={this.onKeyUp} text={this.state.displayValue} />
+    return (
+      <Input {...this.props} text={this.state.displayValue} />
+    )
   }
 }
 
 const Input = (props) => {
   return (
     <React.Fragment>
-      <Focusable onKeyUp={props.onKeyUp}>
-        <rect
-          x={props.x}
-          y={props.y}
-          width={props.width}
-          height={props.height}
-          stroke={'gray'}
-          fill={'darkgray'}
-          rx={4}
-          ry={4}
-          onMouseEnter={(event) => console.log('mouseEnter', event)}
-        />
-        <text
-          x={props.x}
-          y={props.y + (props.height / 2) }
-          fill={'black'}
-          style={{ font: '30px sans-serif' }}
-          tabIndex={0}
-        >{props.text}</text>
-      </Focusable>
+      <rect
+        x={props.x}
+        y={props.y}
+        width={props.width}
+        height={props.height}
+        stroke={'gray'}
+        fill={'darkgray'}
+        rx={4}
+        ry={4}
+        onMouseEnter={(event) => console.log('mouseEnter', event)}
+      />
+      <text
+        x={props.x}
+        y={props.y + (props.height / 2) }
+        fill={'black'}
+        style={{ font: '30px sans-serif' }}
+      >{props.text}</text>
     </React.Fragment>
   )
 }
