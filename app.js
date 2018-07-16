a7b1c486a02beb9d61472a00a9561a76910340fb
Max Harris <harris.max@gmail.com>
2018-07-15 20:52:55 -0500

    revise text inputs, input focuser

--- app.js
+++ app.js
@@ -2,23 +2,91 @@
 
 const App = (props) => {
   const star = <Star width={90} height={94} scale={0.5}/>
+  const inputWidth = 1200
+  const inputHeight = 80
   return (
     <React.Fragment>
       <rect x={0} y={0} width={props.width} height={props.height} fill={'white'} stroke={'cornsilk'} strokeWidth={2} strokeDasharray={'5,5'}/>
       <KeyboardContext.Consumer>
         {
-          keystroke => (
-            <InputFocuser keystroke={keystroke}>
-              <SpacedRay x1={props.width/2} y1={100} x2={props.width/2} y2={props.height} interval={140} showLayout>
-                <Center><InputController x={0} y={0} width={1200} height={80} placeholder={'Full Name'} showLayout={props.showLayout} /></Center>
-                <Center><InputController x={0} y={0} width={1200} height={80} placeholder={'Address'} showLayout={props.showLayout} /></Center>
-                <Center><InputController x={0} y={0} width={1200} height={80} placeholder={'City'} showLayout={props.showLayout} /></Center>
-                <Center><InputController x={0} y={0} width={1200} height={80} placeholder={'State'} showLayout={props.showLayout} /></Center>
-                <Center><InputController x={0} y={0} width={1200} height={80} placeholder={'ZIP Code'} showLayout={props.showLayout} /></Center>
-                <Center><Button x={0} y={0} width={1200} height={80} text={'Sign Up'} fill={'teal'} showLayout={props.showLayout} /></Center>
-              </SpacedRay>
-            </InputFocuser>
-          )
+          getKeystroke => {
+            const keystroke = getKeystroke()
+            return (
+              <InputFocuser
+                tabs={6}
+                keystroke={keystroke}
+                render={focusedIndex => (
+                  <SpacedRay x1={props.width/2} y1={100} x2={props.width/2} y2={props.height} interval={140} showLayout>
+                    <Center>
+                      <InputController
+                         width={inputWidth}
+                         height={inputHeight}
+                         placeholder={'Full Name'}
+                         tabIndex={0}
+                         focusedIndex={focusedIndex}
+                         keystroke={keystroke}
+                      />
+                    </Center>
+                    <Center>
+                      <InputController
+                        width={inputWidth}
+                        height={inputHeight}
+                        placeholder={'Address'}
+                        tabIndex={1}
+                        focusedIndex={focusedIndex}
+                        keystroke={keystroke}
+                      />
+                    </Center>
+                    <Center>
+                      <InputController
+                        width={inputWidth}
+                        height={inputHeight}
+                        placeholder={'City'}
+                        tabIndex={2}
+                        focusedIndex={focusedIndex}
+                        keystroke={keystroke}
+                      />
+                    </Center>
+                    <Center>
+                      <InputController
+                        width={inputWidth}
+                        height={inputHeight}
+                        placeholder={'State'}
+                        tabIndex={3}
+                        focusedIndex={focusedIndex}
+                        keystroke={keystroke}
+                      />
+                    </Center>
+                    <Center>
+                      <InputController
+                        width={inputWidth}
+                        height={inputHeight}
+                        placeholder={'ZIP Code'}
+                        tabIndex={4}
+                        focusedIndex={focusedIndex}
+                        keystroke={keystroke}
+                      />
+                    </Center>
+                    <Center>
+                      <Button
+                        x={0}
+                        y={0}
+                        width={inputWidth}
+                        height={inputHeight}
+                        text={'Sign Up'}
+                        fill={'teal'}
+                        tabIndex={5}
+                        focused={focusedIndex === 5}
+                        keystroke={keystroke}
+
+                        focusedIndex={focusedIndex}
+                      />
+                    </Center>
+                  </SpacedRay>
+                )}
+              />
+            )
+          }
         }
       </KeyboardContext.Consumer>
       <SpacedRay x1={100} y1={100} x2={100} y2={props.height} interval={props.width / 10} showLayout>
--- input.js
+++ input.js
@@ -6,47 +6,33 @@ class InputFocuser extends React.Component {
     super(props)
 
     this.state = {
-      focusIndex: 0
+      focusedIndex: 0
     }
 
     this.handleFocus = this.handleFocus.bind(this)
   }
 
   handleFocus (index) {
-    this.setState({ focusIndex: index })
+    this.setState({ focusedIndex: index })
   }
 
   render () {
-    const keystroke = this.props.keystroke()
+    const { keystroke } = this.props
 
     if (keystroke.code === 'Tab') {
-      const nextFocusedIndex = this.state.focusIndex + (keystroke.shiftKey ? -1 : 1)
-      const nextChildrenLength = this.props.children.length - 1
+      const nextFocusedIndex = this.state.focusedIndex + (keystroke.shiftKey ? -1 : 1)
+      const nextChildrenLength = this.props.tabs - 1
 
       if (nextChildrenLength < nextFocusedIndex) {
-        this.state.focusIndex = 0
+        this.state.focusedIndex = 0
       } else if (nextFocusedIndex < 0) {
-        this.state.focusIndex = nextChildrenLength
+        this.state.focusedIndex = nextChildrenLength
       } else {
-        this.state.focusIndex = nextFocusedIndex
+        this.state.focusedIndex = nextFocusedIndex
       }
     }
 
-    return normalizeChildren(this.props.children).map((child, index) => {
-      if (index === this.state.focusIndex) {
-        return forwardProps(child, {
-          focused: true,
-          keystroke: keystroke,
-          onClick: this.handleFocus,
-          index
-        })
-      }
-
-      return forwardProps(child, {
-        onClick: this.handleFocus,
-        index
-      })
-    })
+    return this.props.render(this.state.focusedIndex)
   }
 }
 
@@ -61,6 +47,9 @@ class InputController extends React.Component {
   componentWillReceiveProps (nextProps) {
     if (!nextProps || !nextProps.keystroke || !nextProps.keystroke.code) { return }
 
+    const focused = nextProps.tabIndex === nextProps.focusedIndex
+    if (!focused) { return }
+
     if (nextProps.keystroke.code === 'Backspace') {
       this.setState({ displayValue: this.state.displayValue.slice(0, this.state.displayValue.length - 1) })
     } else {
@@ -70,7 +59,7 @@ class InputController extends React.Component {
 
   render () {
     return (
-      <Input {...this.props} text={this.state.displayValue} />
+      <Input {...this.props} text={this.state.displayValue} focused={this.props.tabIndex === this.props.focusedIndex} />
     )
   }
 }
@@ -80,8 +69,8 @@ const Input = (props) => {
   return (
     <React.Fragment>
       <rect
-        x={props.x}
-        y={props.y}
+        x={0}
+        y={0}
         width={props.width}
         height={props.height}
         stroke={props.focused ? 'skyblue' : 'gray'}
@@ -93,8 +82,8 @@ const Input = (props) => {
         onClick={(event) => props.onClick(props.index)}
       />
       <text
-        x={props.x + 20}
-        y={props.y + (props.height / 2) + (textHeight / 2)}
+        x={20}
+        y={(props.height / 2) + (textHeight / 2)}
         fill={!props.text ? 'gray' : 'black'}
         style={{ font: '30px sans-serif' }}
       >{!props.text ? props.placeholder : props.text}</text>
