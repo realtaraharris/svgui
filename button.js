d7a1b4f2125f8e31bb4bde6c46cdfae25369ffa2
Max Harris <harris.max@gmail.com>
2018-08-24 21:27:47 -0700

    clean up text measurement magic numbers a little in button component

--- button.js
+++ button.js
@@ -8,22 +8,23 @@ const Label = require('./label')
 const G = require('./shapes/g')
 
 const Button = (props) => {
-  const textWidth = measureTextWidth({
+  const textWidth = 3 * measureTextWidth({ // TODO: why 3x?
     text: props.text,
     fontWeight: 500,
     fontStyle: 'regular',
     fontSize: '30px',
     fontFamily: 'sans-serif'
   })
+  const textHeight = 21
 
   return (
     <React.Fragment>
-        <rect x={0} y={0} width={props.width} height={props.height} rx={15} ry={15} fill={props.fill}
-          stroke={props.focused ? 'skyblue' : props.fill}
-          strokeWidth={props.focused ? 4 : 0}
-          onClick={event => props.onClick()}
-        />
-        <Center width={textWidth * 3} height={21}><Label text={props.text} x={props.width / 2} y={props.height / 2} fill={'white'} /></Center>
+      <rect x={0} y={0} width={props.width} height={props.height} rx={15} ry={15} fill={props.fill}
+        stroke={props.focused ? 'skyblue' : props.fill}
+        strokeWidth={props.focused ? 4 : 0}
+        onClick={event => props.onClick()}
+      />
+      <Center width={textWidth} height={textHeight}><Label text={props.text} x={props.width / 2} y={props.height / 2} fill={'white'} /></Center>
     </React.Fragment>
   )
 }
