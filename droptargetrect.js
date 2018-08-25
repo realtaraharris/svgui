ddfa3f85e336eb25134daf652e31e233ed5e07e6
Max Harris <harris.max@gmail.com>
2018-08-24 21:28:36 -0700

    remove copy-pasta from drop target rect component

--- droptargetrect.js
+++ droptargetrect.js
@@ -1,41 +1,20 @@
 const React = require('react')
 const Rect = require('./shapes/rect')
 
-class DropTargetRect extends React.Component {
-  constructor (props) {
-    super(props)
+const DropTargetRect = (props) => {
+  const { x, y, width, height, name, onDragMove, stroke, fill } = props
 
-    this.state = {
-      mouseDown: false,
-      shapePosition: { x: this.props.x, y: this.props.y },
-      previousMousePosition: { x: 0, y: 0 },
-      dragDelta: { x: 0, y: 0 }
-    }
-
-    this.onDragMove = this.onDragMove.bind(this)
-  }
-
-  onDragMove () {
-    const { x, y, name } = this.props
-    this.props.onDragMove({ x, y }, name)
-  }
-
-  render () {
-    const { width, height, stroke, fill } = this.props
-    const { shapePosition, dragDelta } = this.state
-
-    return (
-      <Rect
-        x={shapePosition.x + dragDelta.x}
-        y={shapePosition.y + dragDelta.y}
-        width={width}
-        height={height}
-        onDragMove={this.onDragMove}
-        stroke={stroke}
-        fill={fill}
-      />
-    )
-  }
+  return (
+    <Rect
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      onDragMove={() => onDragMove({ x, y, width, height }, name)}
+      stroke={stroke}
+      fill={fill}
+    />
+  )
 }
 
 module.exports = DropTargetRect
